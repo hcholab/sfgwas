3a8d691f17210fdf6e39f4791194df44ea9742c6
Hoon Cho <hcho06@gmail.com>
2026-08-10 23:10:30 -0400

    fix

--- gwas/assoc.go
+++ gwas/assoc.go
@@ -1101,7 +1101,11 @@ func (ast *AssocTestPlainMult) computeCovOrthoFactor(cryptoParams *crypto.Crypto
 					outFloat[i] = append([]float64(nil), out.RawRowView(i)...)
 				}
 
-				Aenc, _, _, _ := crypto.EncryptFloatMatrixRow(cryptoParams, outFloat)
+				var Aenc crypto.CipherMatrix
+				if pid == 1 {
+					Aenc, _, _, _ = crypto.EncryptFloatMatrixRow(cryptoParams, outFloat)
+				}
+				Aenc = mpcObj.Network.BroadcastCMat(cryptoParams, Aenc, 1, len(Aenc), len(Aenc[0]))
 				return Aenc
 			},
 		}
