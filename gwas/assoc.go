2d150a75fcdfb6882023b683a041fa736bc688b0
Hoon Cho <hcho06@gmail.com>
2026-08-10 22:16:12 -0400

    Fixed pid=0 error

--- gwas/assoc.go
+++ gwas/assoc.go
@@ -1042,9 +1042,11 @@ func (ast *AssocTestPlainMult) computeCovOrthoFactor(cryptoParams *crypto.Crypto
 
 		// ###### DEBUG ######
 		log.LLvl1("## DEBUG Replacing Sss and Sct with plaintext matrices for testing ##")
-		Sfloat := LoadMatrixFromFileFloat(ast.general.CachePath("cholesky_S_truth.txt"), ',')
-		Sct, _, _, _ = crypto.EncryptFloatMatrixRow(cryptoParams, Sfloat)
-		Sss = mpcObj.CMatToSS(cryptoParams, mpcObj.GetRType(), Sct, 1, len(Sct), len(Sct[0]), len(Sss[0]))
+		if pid > 0 {
+			Sfloat := LoadMatrixFromFileFloat(ast.general.CachePath("cholesky_S_truth.txt"), ',')
+			Sct, _, _, _ = crypto.EncryptFloatMatrixRow(cryptoParams, Sfloat)
+			Sss = mpcObj.CMatToSS(cryptoParams, mpcObj.GetRType(), Sct, 1, len(Sct), len(Sct[0]), len(Sss[0]))
+		}
 		// ###################
 
 		return covOrthoFactor{
