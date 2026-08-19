9fd864e1b42f86a53360ae2fd89a9ef1deb98b28
Hoon Cho <hhcho@users.noreply.github.com>
2026-08-19 19:03:46 -0400

    Trying unit var

--- gwas/assoc.go
+++ gwas/assoc.go
@@ -1279,7 +1279,7 @@ func (ast *AssocTestPlainMult) GetAssociationStatsPlainMult() (crypto.CipherMatr
 		}
 		for i := 0; i < ncov; i++ {
 			floats.AddConst(-z1[i], Zt.RawRowView(i))
-			floats.Scale(10.0/zz1[i], Zt.RawRowView(i))
+			floats.Scale(1.0/zz1[i], Zt.RawRowView(i))
 		}
 	}
 
